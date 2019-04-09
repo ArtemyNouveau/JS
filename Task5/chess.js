5725695c178077fa0b0868f51b380a4d5dd6afd3
ArtemyNouveau <47831303+ArtemyNouveau@users.noreply.github.com>
2019-04-10 00:52:15 +0300

    Add files via upload

---
+++ Task5/chess.js
@@ -0,0 +1,11 @@
+const Figures = [
+    "<i class=\"fas fa-chess-rook\"></i>",
+    "<i class=\"fas fa-chess-knight\"></i>",
+    "<i class=\"fas fa-chess-bishop\"></i>",
+    "<i class=\"fas fa-chess-queen\"></i>",
+    "<i class=\"fas fa-chess-king\"></i>",
+    "<i class=\"fas fa-chess-bishop\"></i>",
+    "<i class=\"fas fa-chess-knight\"></i>",
+    "<i class=\"fas fa-chess-rook\"></i>",
+];
+const Pawn = "<i class=\"fas fa-chess-pawn\"></i>";
